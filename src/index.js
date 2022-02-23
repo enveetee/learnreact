efa5d86a50f06854628e52f02be1a4a8902c461e
enveetee <nigel@couriersystems.co.uk>
2022-02-23 17:54:14 +0000

    test

--- src/index.js
+++ src/index.js
@@ -1,27 +1,7 @@
+const elvenShieldRecipe={
+   leatherStrips:2,
+   ironIngot:1,
+   refinedMoonstone:4
+}
 
-//require('file-loader?name=[name].[ext]!./index.html');
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-
-
-const d = new Date()
-let seconds=d.getSeconds();
-
-
-const page = (
-   <div>
-
-   <img src='/assets/images/react.png' width='40px' alt="asdf" />
-      <h1>Funs factds aboutd sResacat</h1>
-   <ol>
-      <li><h4>Was first released in 2013</h4></li>
-      <li>Was originally created by Jordan Walke</li>
-      <li>Has well over 100K stars on Github</li>
-      <li>Is maintained by Facebookd</li>
-      <li>sPowers thousands of enterprice apps, including mobidle apps</li>
-   </ol>
-   </div>)
-const appElement=document.getElementById('root');
-document.getElementsByTagName("title")[0].text=seconds;
-ReactDOM.render(page,appElement);
+console.log(elvenShieldRecipe)
